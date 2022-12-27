ccd4399530ef46e56e9ffee9803aacd08e29b63d
Ozzy <ozzyalvarado96@gmail.com>
2022-12-27 18:26:43 -0500

    server link updated

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch('https://askmeanything-zxhj.onrender.com', {
+  const response = await fetch('https://askmeanything-hegl.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
