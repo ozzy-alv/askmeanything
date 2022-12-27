e48a6c5d63ff76494a3a3539d29b23911488e96e
Ozzy <ozzyalvarado96@gmail.com>
2022-12-27 17:36:53 -0500

    changes

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
-import * as dotenv from 'dotenv';
 import express from 'express';
+import * as dotenv from 'dotenv';
 import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
 
@@ -15,32 +15,33 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.status(200).send({
-    message: 'hello from ChatGPT CodeX',
+    message: 'Hello from CodeX!',
   });
 });
 
 app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
+
     const response = await openai.createCompletion({
       model: 'text-davinci-003',
       prompt: `${prompt}`,
-      temperature: 0,
-      max_tokens: 3000,
-      top_p: 1,
-      frequency_penalty: 0.5,
-      presence_penalty: 0,
+      temperature: 0, // Higher values means the model will take more risks.
+      max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
+      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
+      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
     });
 
     res.status(200).send({
       bot: response.data.choices[0].text,
     });
   } catch (error) {
-    console.log(error);
-    res.status(500).send({ error });
+    console.error(error);
+    res.status(500).send(error || 'Something went wrong');
   }
 });
 
-app.listen(5000, () => console.log(`listening on... http://localhost:5000`));
+app.listen(5000, () => console.log('server started on port'));
