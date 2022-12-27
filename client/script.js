87ea5dc76c200e6a5a2d7fa438432b6a32775193
Ozzy <ozzyalvarado96@gmail.com>
2022-12-27 17:02:17 -0500

    added server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from the ChatGPT server
 
-  const response = await fetch(`http://localhost:5000`, {
+  const response = await fetch(`https://askmeanything-zxhj.onrender.com/`, {
     method: 'POST',
     headers: {
       'content-type': 'application/json',
